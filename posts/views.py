97e2587320ce962a4576a61056659b985958817f
Gabriel Roger <gabriel.nroger@gmail.com>
2019-12-03 21:11:54 -0300

    arquivos estáticos

--- posts/views.py
+++ posts/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    nome = 'Gabriel'
+    nome = 'groger'
+    # posts = {
+    #     'Gabriel' : 'Narnia para projetos ágeis',
+    #     'Vinicius': 'IA para comer biscoitos',
+    #     'Ana Lu': 'BTS é muito bondoso',
+    #     'Downtown': 'Não seja maníaco'
+    # }
+
+    lista = ['Roré', 'Generro', 'Anlu', 'DOwnT0wn']
     contexto = {
-        'nome': nome
+        'nome': nome,
+        'lista': lista
     }
     return render(request, 'home.html', contexto)
