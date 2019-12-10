d24430d12a357b521ebecf99c81d80bca7b0ccf0
diandra <di.andrade90@gmail.com>
2019-12-10 12:18:04 -0300

    Adicionar models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Postagem(models.Model):
+    opcoes_tema = [('RO', 'Romance'),
+    ('TE', 'Tecnologia'),
+    ('NO', 'Novidades'),
+    ]
+
+    nome = models.CharField(max_length=50)
+    ativo = models.BooleanField(default=True)
+    idade = models.IntegerField(default=1)
+    tema  = models.CharField(max_length=2, choices=opcoes_tema)
+    
+    
+class Pedido(models.Model):
+    metodo_pagamento = [
+        ('AV', 'Pagamento à vista'),
+        ('P2', 'Parcelado em 2x'),
+        ('P3', 'Parcelado em 3x'),
+    ]
+    nome = models.CharField(max_length=140)
+    email = models.EmailField()
+    cartao = models.IntegerField()
+    pagamento = models.CharField(max_length=2, choices=metodo_pagamento)
